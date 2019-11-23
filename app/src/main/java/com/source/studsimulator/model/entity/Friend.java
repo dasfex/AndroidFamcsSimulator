94af858de2d5c456453f9333030fcf855df3658f
KhodorVan <ixodor@mail.ru>
2019-11-24 00:59:14 +0300

    spinner begin

--- app/src/main/java/com/source/studsimulator/model/entity/Friend.java
+++ app/src/main/java/com/source/studsimulator/model/entity/Friend.java
@@ -1,8 +1,22 @@
 package com.source.studsimulator.model.entity;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class Friend {
 
     private int friendshipLevel = 0;
     private int weeksWithoutContact = 0;
     private double busyProbability = 0.2;
+    private TextView name;
+    private ImageView photo;
+
+    public Friend(int friendshipLevel, int weeksWithoutContact, double busyProbability,
+                  TextView name, ImageView photo) {
+        this.friendshipLevel = friendshipLevel;
+        this.weeksWithoutContact = weeksWithoutContact;
+        this.busyProbability = busyProbability;
+        this.photo = photo;
+        this.name = name;
+    }
 }
---
+++ app/src/main/java/com/source/studsimulator/ui/widgets/FriendSpinner.java
@@ -0,0 +1,10 @@
+/*package com.source.studsimulator.ui.widgets;
+
+import android.widget.Spinner;
+
+public class FriendSpinner extends Spinner {
+
+
+
+}
+*/
