d768da09895891c8ed2c049bc2f4807d1e76ce45
KhodorVan <ixodor@mail.ru>
2019-11-24 01:23:53 +0300

    path to success(just begin)

--- app/src/main/java/com/source/studsimulator/ui/fragments/HobbyFragment.java
+++ app/src/main/java/com/source/studsimulator/ui/fragments/HobbyFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
