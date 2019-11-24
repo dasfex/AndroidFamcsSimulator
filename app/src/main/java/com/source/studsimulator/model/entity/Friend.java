5028a144c8f9200516625069f16dbd60221d11cf
KhodorVan <ixodor@mail.ru>
2019-11-24 12:33:54 +0300

    add friends list in spinner

--- app/src/main/java/com/source/studsimulator/model/entity/Friend.java
+++ app/src/main/java/com/source/studsimulator/model/entity/Friend.java
@@ -8,15 +8,40 @@ public class Friend {
     private int friendshipLevel = 0;
     private int weeksWithoutContact = 0;
     private double busyProbability = 0.2;
-    private TextView name;
-    private ImageView photo;
+    private String name;
+    private int photoId;
 
     public Friend(int friendshipLevel, int weeksWithoutContact, double busyProbability,
-                  TextView name, ImageView photo) {
+                  String name, int photoId) {
         this.friendshipLevel = friendshipLevel;
         this.weeksWithoutContact = weeksWithoutContact;
         this.busyProbability = busyProbability;
-        this.photo = photo;
         this.name = name;
+        this.photoId = photoId;
+    }
+
+    public int getFriendshipLevel() {
+        return friendshipLevel;
+    }
+
+    public int getWeeksWithoutContact() {
+        return weeksWithoutContact;
+    }
+
+    public double getBusyProbability() {
+        return busyProbability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhotoId() {
+        return photoId;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
