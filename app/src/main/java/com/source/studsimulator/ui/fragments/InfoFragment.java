f2374abc3ef5743e5c9222a1cdb950e016dd96ba
yaninasan10 <yaninasan@tut.by>
2019-11-27 00:31:58 +0300

    i'm sorry

--- app/src/main/java/com/source/studsimulator/ui/fragments/InfoFragment.java
+++ app/src/main/java/com/source/studsimulator/ui/fragments/InfoFragment.java
@@ -1,19 +1,27 @@
 package com.source.studsimulator.ui.fragments;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.source.studsimulator.R;
 
 public class InfoFragment extends Fragment {
 
     private Button newWeekButton;
+    private ImageView photo;
 
     private OnInformationFragmentListener activityListener;
 
@@ -21,12 +29,34 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.info_fragment_activity, null);
-
+        photo = view.findViewById(R.id.artyomTheStudent);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showToast(v);
+            }
+        });
         newWeekButton = view.findViewById(R.id.newWeekButton);
 
         newWeekButton.setOnClickListener(v -> activityListener.onNewWeekClicked());
 
         return view;
+    }
+
+    public void showToast(View view) {
+        Toast toast3 = Toast.makeText(view.getContext(),
+                R.string.art, Toast.LENGTH_SHORT);
+        toast3.setGravity(Gravity.NO_GRAVITY, 0, 0);
+
+        LinearLayout toastContainer = (LinearLayout) toast3.getView();
+            toastContainer.setBackgroundColor(Color.TRANSPARENT);
+        ImageView artImage = new ImageView(view.getContext());
+        artImage.setImageResource(R.drawable.hello_artyom);
+        artImage.setMaxWidth(300);
+        artImage.setMaxHeight(300);
+            toastContainer.addView(artImage, 0);
+
+        toast3.show();
     }
 
     @Override
