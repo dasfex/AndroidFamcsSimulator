7d8a30eb6f3f06aef2cfffd53170d6546dac0860
KhodorVan <ixodor@mail.ru>
2019-12-03 20:33:07 +0300

    add exit and settings

--- app/src/main/java/com/source/studsimulator/ui/activity/MainActivity.java
+++ app/src/main/java/com/source/studsimulator/ui/activity/MainActivity.java
@@ -12,16 +12,33 @@ import com.source.studsimulator.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button newGameButton;
+    private Button settingsButton;
+    private Button exitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        Button newGameButton = findViewById(R.id.newGameButton);
+        newGameButton = findViewById(R.id.newGameButton);
         newGameButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, GameLobbyActivity.class);
             startActivity(intent);
+        });
+
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(v -> {
+            super.onDestroy();
+            System.runFinalization();
+            System.exit(0);
         });
     }
 }
